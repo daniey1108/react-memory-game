bb4e69b7ba77c2cc693f14e1458e9ff861f4e8bc
Dani Allan <30298565+awaywithdreams@users.noreply.github.com>
2021-06-16 21:19:19 -0400

    hopefully fixing the linter errors and adding the pipeline operator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Index extends React.Component {
     render () {
         return (
-            <>
+          <>
             <h1>Hopefully this will deploy fine!</h1>
-            </>
+          </>
         );
     }
 }
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: "./build",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: "./build",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: 'url-loader',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./index.html'),
+        }),
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template:  path.resolve('./index.html'),
-    }),
-  ]
 };
